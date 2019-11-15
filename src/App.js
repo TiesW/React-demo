26d6011a1eff977500512fbec0ae58c021a7bf59
TiesW <tieswijnker@gmail.com>
2019-11-15 14:31:00 +0100

    React demo met kattentrivia via axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Student from "./Student/student";
+import KlasLokaal from "./Student/klaslokaal";
 
 function App(props) {
   return (
@@ -12,6 +13,7 @@ function App(props) {
         <Student studie="Anthropoligie" afgestudeerd="niet"> Een hier een extra zin. </Student>
         <Student studie="Wiskunde" afgestudeerd="wel"> Nog een zin. </Student>
         <Student studie="Geneeskunde" afgestudeerd="niet"> Ook hier een extra zin. </Student>
+        <KlasLokaal></KlasLokaal>
       </header>
     </div>
   );
--- src/Student/klaslokaal.js
+++ src/Student/klaslokaal.js
@@ -1,5 +1,35 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class KlasLokaal extends Component{
-    Yoyo
+    
+    state = {
+        lokalen : [ { lokaalnummer : "12" }, { lokaalnummer : "3"} ],
+        kattentekst : "test"
+    }
+
+    klikken = () => {
+        this.setState({
+            lokalen : [ { lokaalnummer : "2"}, { lokaalnummer : "4"} ]
+        })
+    }
+
+    halen = ()=>{
+       axios.get('https://cat-fact.herokuapp.com/facts/random').then(response => this.setState( { kattentekst : response.data.text }));
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Kattenfeitje: {this.state.kattentekst}</h1>
+                <button onClick={this.halen}>Klik hier voor een kattenfeitje</button>
+                <h1>Bekijk hier het klaslokaal: {this.state.lokalen[0].lokaalnummer}</h1>
+                <h2>Bekijk hier het klaslokaal: {this.state.lokalen[1].lokaalnummer}</h2>
+                <button onClick={this.klikken}>Klik hier voor een ander klaslokaal</button>
+            </div>
+        )
+    }
 }
+
+export default KlasLokaal;
